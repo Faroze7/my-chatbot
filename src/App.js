68f6c63d8d90f829f99e4b2e849e1be3cbd65c15
Faroze7 <farozemohamed@gmail.com>
2025-11-14 19:59:31 +0400

    Update App.js
    change logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Chatbot from "./components/Chatbot";
 import "./App.css";
-import logo from "./assets/4.png";
+import logo from "./assets/faroze-logo.png";
 
 function App() {
   return (
